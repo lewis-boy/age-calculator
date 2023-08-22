f71a543dc6577d5b5037f152967a329c23252f4a
Luis Escobar <luisaeniebla@gmail.com>
2023-08-21 19:54:53 -0700

    finished getting the config setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,14 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Poppins } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+export const poppins = Poppins({
+    subsets: ['latin'],
+    weight: ['400', '800', '900'],
+    style: ['normal', 'italic'],
+    variable: '--font-poppins'
+})
+
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,8 +21,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={`${poppins.variable}`}>
+      <body>{children}</body>
     </html>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
 import type { Config } from 'tailwindcss'
 
+
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,6 +8,32 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      'desktop': '1440px'
+    },
+
+    colors: {
+      'purple': 'hsl(259, 100%, 65%)',
+      'light-red': 'hsl(0, 100%, 67%)',
+      'white': 'hsl(0, 0%, 100%)',
+      'off-white': 'hsl(0, 0%, 94%)',
+      'light-grey': 'hsl(0, 0%, 86%)',
+      'smokey-grey': 'hsl(0, 1%, 44%)',
+      'off-black': 'hsl(0, 0%, 8%)',
+    },
+
+    fontSize: {
+      'mobile-lg' : ['56px', {lineHeight: '110%', letterSpacing: '-2%'}], 
+      'mobile-md' : ['20px', {lineHeight: '100%', letterSpacing: '1%'}], 
+      'mobile-sm' : ['12px', {lineHeight: '100%', letterSpacing: '25%'}], 
+      'mobile-error' : ['12px', {lineHeight: '100%', letterSpacing: '0%'}], 
+    },
+
+    fontFamily: {
+      'poppins': ['var(--font-poppins)']
+    },
+
+
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
@@ -15,6 +42,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms')],
 }
 export default config
