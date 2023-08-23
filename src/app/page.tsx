e1728aac4036889b02fb946e666e340a903c42e4
Luis Escobar <luisaeniebla@gmail.com>
2023-08-22 20:20:28 -0700

    Mostly done. Effect hooks and form updates were not working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import AgeCalculator from '@/components/AgeCalculator'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 font-poppins italic">
-      Main
+      <AgeCalculator />
     </main>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,47 +1,45 @@
-import type { Config } from 'tailwindcss'
-
+import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     screens: {
-      'desktop': '1440px'
+      desktop: "1440px",
     },
 
     colors: {
-      'purple': 'hsl(259, 100%, 65%)',
-      'light-red': 'hsl(0, 100%, 67%)',
-      'white': 'hsl(0, 0%, 100%)',
-      'off-white': 'hsl(0, 0%, 94%)',
-      'light-grey': 'hsl(0, 0%, 86%)',
-      'smokey-grey': 'hsl(0, 1%, 44%)',
-      'off-black': 'hsl(0, 0%, 8%)',
+      purple: "hsl(259, 100%, 65%)",
+      "light-red": "hsl(0, 100%, 67%)",
+      white: "hsl(0, 0%, 100%)",
+      "off-white": "hsl(0, 0%, 94%)",
+      "light-grey": "hsl(0, 0%, 86%)",
+      "smokey-grey": "hsl(0, 1%, 44%)",
+      "off-black": "hsl(0, 0%, 8%)",
     },
 
     fontSize: {
-      'mobile-lg' : ['56px', {lineHeight: '110%', letterSpacing: '-2%'}], 
-      'mobile-md' : ['20px', {lineHeight: '100%', letterSpacing: '1%'}], 
-      'mobile-sm' : ['12px', {lineHeight: '100%', letterSpacing: '25%'}], 
-      'mobile-error' : ['12px', {lineHeight: '100%', letterSpacing: '0%'}], 
+      "mobile-lg": ["56px", { lineHeight: "110%", letterSpacing: "-.02em" }],
+      "mobile-md": ["20px", { letterSpacing: ".01em" }],
+      "mobile-sm": ["12px", { letterSpacing: ".25em" }],
+      "mobile-error": ["12px", { lineHeight: "100%" }],
     },
 
     fontFamily: {
-      'poppins': ['var(--font-poppins)']
+      poppins: ["var(--font-poppins)"],
     },
-
 
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [require("@tailwindcss/forms")],
 }
 export default config
